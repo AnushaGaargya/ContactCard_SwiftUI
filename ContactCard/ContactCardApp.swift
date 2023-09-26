512734933d34d546f8f6916acc7bdb53ac0f5fdf
Anusha Gaargya <anushasgarg@gmail.com>
2023-09-26 22:36:44 +0530

    Initial Commit

---
+++ ContactCard/ContactCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactCardApp.swift
+//  ContactCard
+//
+//  Created by Anusha S on 26/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ContactCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContactCard/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  ContactCard
+//
+//  Created by Anusha S on 26/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Color(red: 0.95, green: 0.61, blue: 0.07)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+               
+                Image("dp")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                    .clipShape(Circle())
+                Text("Anusha")
+                    .font(Font.custom("SedgwickAveDisplay-Regular", size: 45))
+                    .bold()
+                Text("iOS Developer")
+                    .font(.title)
+                    .bold()
+                    
+                
+                
+            }
+            .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
